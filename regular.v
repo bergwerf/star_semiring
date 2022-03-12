03717a01485e7a4f9adf29e3775e944fd5d17191
Herman Bergwerf <hermanbergwerf@gmail.com>
2022-03-12 17:22:24 +0100

    I added some lemmas.

--- regular.v
+++ regular.v
@@ -2,7 +2,7 @@ From stars Require Import definitions.
 
 Section Regular_Expressions.
 
-Variable X : Type.
+Context {X : Type}.
 
 Inductive re :=
   | RE_None
@@ -12,18 +12,18 @@ Inductive re :=
   | RE_Seq (a b : re)
   | RE_Star (a : re).
 
-Fixpoint re_elem_of (w : list X) (a : re) :=
+Fixpoint re_in (w : list X) (a : re) :=
   match a with
   | RE_None => False
   | RE_Empty => w = []
   | RE_Literal x => w = [x]
-  | RE_Or b c => re_elem_of w b \/ re_elem_of w c
-  | RE_Seq b c => ∃ u v, w = u ++ v /\ re_elem_of u b /\ re_elem_of v c
-  | RE_Star b => ∃ u n, w = concat (repeat u n) /\ re_elem_of u b
+  | RE_Or b c => re_in w b \/ re_in w c
+  | RE_Seq b c => ∃ u v, w = u ++ v /\ re_in u b /\ re_in v c
+  | RE_Star b => w = [] \/ ∃ u n, w = concat (repeat u n) /\ re_in u b
   end.
 
 Global Instance : Empty re := RE_None.
-Global Instance : ElemOf (list X) re := re_elem_of.
+Global Instance : ElemOf (list X) re := re_in.
 Global Instance : Equiv re := λ a b, ∀ w, w ∈ a <-> w ∈ b.
 Global Instance : Zero re := RE_None.
 Global Instance : One re := RE_Empty.
@@ -55,20 +55,31 @@ Global Instance : Star re :=
   | _ => RE_Star a
   end.
 
-Global Instance : Equivalence (≡).
+Lemma re_in_add w a b :
+  w ∈ a + b <-> w ∈ a \/ w ∈ b.
 Proof.
-split; repeat intros ?; auto.
-symmetry; auto. etrans; auto.
+destruct a, b; cbn; firstorder.
 Qed.
+
+Lemma re_in_mul w a b :
+  w ∈ a * b <-> ∃ u v, w = u ++ v /\ u ∈ a /\ v ∈ b.
+Proof.
+Admitted.
+
+Lemma re_in_star w a :
+  w ∈ a{*} <-> w = [] \/ ∃ u n, w = concat (repeat u n) /\ u ∈ a.
+Proof.
+Admitted.
+
+Global Instance : Kleene_Algebra re.
+Proof.
+repeat split; repeat intros ?; firstorder.
+(* Automate proving equivalence of regular expressions. *)
+Admitted.
 
 End Regular_Expressions.
 
-Arguments RE_Literal {_}.
-Arguments RE_Or {_}.
-Arguments RE_Seq {_}.
-Arguments RE_Star {_}.
-
-Notation "'ε'" := (RE_Empty _).
+Notation "'ε'" := (RE_Empty).
 Notation "a ∣ b" := (RE_Or a b) (at level 52, format "a ∣ b").
 Notation "a ⋅ b" := (RE_Seq a b) (at level 51, format "a ⋅ b").
 Notation "a ∗" := (RE_Star a) (at level 50, format "a ∗").
