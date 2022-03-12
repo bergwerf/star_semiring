ab5c1fa417d90c355b608972996ee4b7323dedf1
Herman Bergwerf <hermanbergwerf@gmail.com>
2022-03-12 22:29:01 +0100

    I proved that `re` forms a Kleene algebra.

--- regular.v
+++ regular.v
@@ -170,11 +170,20 @@ Proof. firstorder. Qed.
 Global Instance : Proper ((≡) ==> (≡) ==> (≡)) RE_Seq.
 Proof. firstorder. Qed.
 
+Global Instance : Proper ((≡) ==> (≡)) RE_Star.
+Proof.
+intros a b Hab w; cbn; split; intros [->|(vs&->&H)]; try (left; done); right;
+exists vs; split; try done; eapply Forall_impl; try apply H; apply Hab.
+Qed.
+
 Global Instance : Proper ((≡) ==> (≡) ==> (≡)) (@add re _).
 Proof. intros a b Hab c d Hcd; rewrite ?equiv_re_add, Hab, Hcd; done. Qed.
 
 Global Instance : Proper ((≡) ==> (≡) ==> (≡)) (@mul re _).
 Proof. intros a b Hab c d Hcd; rewrite ?equiv_re_mul, Hab, Hcd; done. Qed.
+
+Global Instance : Proper ((≡) ==> (≡)) (@star re _).
+Proof. intros a b Hab; rewrite ?equiv_re_star, Hab; done. Qed.
 
 Local Ltac expand := rewrite ?equiv_re_add, ?equiv_re_mul, ?equiv_re_star.
 
@@ -182,11 +191,13 @@ Global Instance : Kleene_Algebra re.
 Proof.
 split. split. split. c. split. 1,3: split. 1,4: split. 1,3: c.
 - intros a b c; expand; firstorder.
-- intros a b c; expand; intros w; cbn; firstorder; subst.
-  + exists (x ++ x1), x2; repeat split; try done.
-    apply app_assoc. exists x, x1; done.
-  + exists x1, (x2 ++ x0); repeat split; try done.
-    symmetry; apply app_assoc. exists x2, x0; done.
+- intros a b c; expand; intros w; cbn; split; intros (u&v&->&H).
+  + destruct H as (Ha&v1&v2&->&Hv1&Hv2).
+    exists (u ++ v1), v2; repeat split; try done.
+    apply app_assoc. exists u, v1; done.
+  + destruct H as ((u1&u2&->&Hu1&Hu2)&Hv).
+    exists u1, (u2 ++ v); repeat split; try done.
+    symmetry; apply app_assoc. exists u2, v; done.
 - intros a; expand; firstorder.
 - intros a; expand; firstorder.
 - intros a; expand; apply re_seq_1_l.
@@ -199,8 +210,15 @@ split. split. split. c. split. 1,3: split. 1,4: split. 1,3: c.
 - intros a; expand; apply re_star_expand_left.
 - intros a; expand; apply re_star_expand_right.
 - intros a; expand; firstorder.
-- intros a b; expand. admit.
-- intros a b; expand. admit.
-Admitted.
+- intros a b; expand; intros H w; cbn; split; [|auto].
+  intros [(u&v&->&[->|(us&->&Hus)]&Hv)|Hw]; simplify_list_eq; try done.
+  induction us as [|u us]; simplify_list_eq. done. decompose_Forall.
+  apply H; cbn; left; eexists; eexists; auto.
+- intros a b; expand; intros H w; cbn; split; [|auto].
+  intros [(u&v&->&Hu&[->|(vs&->&Hvs)])|Hw]; simplify_list_eq; try done.
+  induction vs as [|v vs] using rev_ind; simplify_list_eq. done.
+  rewrite concat_app; simplify_list_eq; rewrite app_assoc.
+  decompose_Forall; apply H; cbn; left; eexists; eexists; auto.
+Qed.
 
 End Regular_Expressions.
